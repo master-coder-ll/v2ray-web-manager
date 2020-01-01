5192c297fc50d8a2a4804a6ab32a11da1047eda0
master-coder-ll <kxsw.send@gmail.com>
2020-01-01 13:52:48 +0800

    init

--- vpn-admin/src/main/java/com/jhl/admin/ManagerApplication.java
+++ vpn-admin/src/main/java/com/jhl/admin/ManagerApplication.java
@@ -2,8 +2,6 @@ package com.jhl.admin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.google.code.kaptcha.impl.DefaultKaptcha;
-import com.google.code.kaptcha.util.Config;
 import com.jhl.admin.util.SimpleJpaRepositoryImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,7 +21,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.Properties;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = SimpleJpaRepositoryImpl.class)
@@ -64,34 +61,6 @@ public class ManagerApplication {
         return restTemplate;
     }
 
-   /* @Bean
-    public DefaultKaptcha buildDefaultKaptcha() {
-        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
-        Properties properties = new Properties();
-        // 图片边框
-        properties.setProperty("kaptcha.border", "yes");
-        // 边框颜色
-        // properties.setProperty("kaptcha.border.color", "105,179,90");
-        // 字体颜色
-        properties.setProperty("kaptcha.textproducer.font.color", "red");
-        // 图片宽
-        properties.setProperty("kaptcha.image.width", "110");
-        // 图片高
-        properties.setProperty("kaptcha.image.height", "40");
-        // 字体大小
-        properties.setProperty("kaptcha.textproducer.font.size", "30");
-        // session key
-        properties.setProperty("kaptcha.session.key", "code");
-        // 验证码长度
-        properties.setProperty("kaptcha.textproducer.char.length", "4");
-        // 字体
-        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
-        Config config = new Config(properties);
-        defaultKaptcha.setConfig(config);
-
-        return defaultKaptcha;
-    }
-*/
    /* @Override
     public void run(ApplicationArguments args) throws Exception {
       *//*  User newUser = User.builder().email("1@qq.com").nickName("test1").role("admin").password("1234").build();

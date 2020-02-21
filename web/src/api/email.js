ca169049fd908bce41f9fe900bb380a62ac7376e
master-coder-ll <59392396+master-coder-ll@users.noreply.github.com>
2020-02-21 21:19:19 +0800

    rm web

--- web/src/api/email.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function sendEmail(email, type,inviteCode) {
-  return request({
-    url: '/user/send-email',
-    method: 'get',
-    params: { 'email': email, 'type': type ,'inviteCode':inviteCode}
-  })
-}

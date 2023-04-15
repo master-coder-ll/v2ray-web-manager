26f774fd914020127bdb1b9def8fd32be61140ae
master-coder-ll <kxsw.send@gmail.com>
2023-04-15 09:12:11 +0800

    release1

--- common/src/main/java/com/ljh/common/utils/V2RayPathEncoder.java
+++ common/src/main/java/com/ljh/common/utils/V2RayPathEncoder.java
@@ -4,9 +4,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class V2RayPathEncoder {
     private V2RayPathEncoder() {
-
     }
-
     public  static  String encoder(String account,String host,String password){
 
         return DigestUtils.md5Hex(account+host+password);
